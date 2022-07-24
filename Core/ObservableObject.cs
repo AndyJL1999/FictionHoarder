02ff33e9cb9bc31eca20c155653119cecf6df7ff
AndyJL1999 <andylainez115@hotmail.com>
2022-07-24 19:41:26 -0400

    Add project files.

---
+++ Core/ObservableObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FictionHoarder.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName]string name = null)
+        {
+            //if ProperyChanged isn't null -> Invoke
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
