3e0135340501085d196340f6fadef9a782c9c09d
AndyJL1999 <andylainez115@hotmail.com>
2022-11-15 18:25:35 -0500

    --Modified Stored Procedure - "spUser_GetUserByNameOrEmail"
    --Added Stored Procedure - "spUser_GetUserById"

---
+++ FictionDb/dbo/StoredProcedure/User_Procedures/spUser_GetUserById.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spUser_GetUserById]
+	@Id int
+AS
+begin
+	select [User].Id, [User].Username, [User].Email, [User].PasswordHash, [User].PasswordSalt
+	from dbo.[User]
+	where Id = @Id;
+end
--- FictionDb/dbo/StoredProcedure/User_Procedures/spUser_GetUserByNameOrEmail.sql
+++ FictionDb/dbo/StoredProcedure/User_Procedures/spUser_GetUserByNameOrEmail.sql
@@ -2,7 +2,7 @@
 	@NameOrEmail nvarchar(100)
 AS
 begin
-	select [User].Username, [User].Email, [User].PasswordHash, [User].PasswordSalt
+	select [User].Id, [User].Username, [User].Email, [User].PasswordHash, [User].PasswordSalt
 	from dbo.[User]
 	where Username = @NameOrEmail or Email = @NameOrEmail;
 end
