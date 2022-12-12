c87e7fefa3639c711532c102a07133f009eac907
AndyJL1999 <andylainez115@hotmail.com>
2022-12-11 20:29:03 -0500

    --Added new event aggregator --This event is used to move a set of stories to another view model

---
+++ FictionUI_Library/EventAggregators/GetRecentHistoryEvent.cs
@@ -0,0 +1,14 @@
+﻿using FictionUI_Library.Models;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FictionUI_Library.EventAggregators
+{
+    public class GetRecentHistoryEvent : PubSubEvent<IEnumerable<StoryModel>>
+    {
+    }
+}
