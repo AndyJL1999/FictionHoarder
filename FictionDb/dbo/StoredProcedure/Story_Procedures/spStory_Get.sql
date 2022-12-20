43cff587585bde4a5fef9294fe3ff7dd4db07ef7
AndyJL1999 <andylainez115@hotmail.com>
2022-12-20 18:24:01 -0500

    --Changed sort condition in spStory_Get stored procedure

--- FictionDb/dbo/StoredProcedure/Story_Procedures/spStory_Get.sql
+++ FictionDb/dbo/StoredProcedure/Story_Procedures/spStory_Get.sql
@@ -1,8 +1,10 @@
 ﻿CREATE PROCEDURE [dbo].[spStory_Get]
-	@Id int
+	@Title nvarchar(50),
+	@Author nvarchar(50), 
+	@EpubFile nvarchar(max)
 AS
 begin
 	select [Story].Id, [Story].Title, [Story].Author, [Story].Summary, [Story].Chapters, [Story].EpubFile
 	from dbo.[Story]
-	where Id = @Id;
+	where Title = @Title and Author = @Author and EpubFile = @EpubFile
 end
